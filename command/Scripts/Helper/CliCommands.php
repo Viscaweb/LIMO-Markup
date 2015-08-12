6f390a54c8ef83c7f11649dfbd37e4ca9d07c8b3
Jonas HAOUZI <jonas@viscaweb.com>
2015-08-12 23:22:51 +0200

    LIMO-25 COMPILATION Using of Inotify instead of FSWatch for Linux

--- command/Scripts/Helper/CliCommands.php
+++ command/Scripts/Helper/CliCommands.php
@@ -6,9 +6,12 @@ namespace Scripts\Helper;
  */
 class CliCommands
 {
-    const COMMAND_FSWATCH = 'fswatch-run %s %s %s';
+    const COMMAND_INFINITE_LOOP = "while true\ndo\n%s\ndone";
+    const COMMAND_FSWATCH = 'fswatch-run %s %s %s -i=%s';
+    const COMMAND_INOTIFY = 'inotifywait -qq -r -e modify -e move -e create -e delete %s && %s';
     const COMMAND_RUN_TASK = 'cd %s && php application.php %s %s %s';
     const COMMAND_MDL_GULP_TASK = 'cd %s && gulp %s';
+    const COMMAND_COMMON = '%s %s';
 
     const ARG_AS_BG = '> /dev/null 2>&1 &';
 
@@ -17,7 +20,8 @@ class CliCommands
      *
      * @return string
      */
-    static public function run($command){
+    static public function run($command)
+    {
         return shell_exec($command);
     }
 
@@ -35,12 +39,32 @@ class CliCommands
         $asBackground = false,
         $returnCommand = false
     ) {
-        $command = sprintf(
-            self::COMMAND_FSWATCH,
-            escapeshellarg($path),
-            escapeshellarg($script),
-            $asBackground ? self::ARG_AS_BG : ''
-        );
+        if (System::isUbuntu()) {
+            $commandInotify = sprintf(
+                self::COMMAND_INOTIFY,
+                escapeshellarg($path),
+                escapeshellarg($script)
+            );
+            $commandInotifyInfinite = sprintf(
+                self::COMMAND_INFINITE_LOOP,
+                $commandInotify
+            );
+            $commandInotifyInfiniteFile = self::createExecutableFileFromCommand(
+                $commandInotifyInfinite
+            );
+            $command = sprintf(
+                self::COMMAND_COMMON,
+                $commandInotifyInfiniteFile,
+                $asBackground ? self::ARG_AS_BG : ''
+            );
+        } else {
+            $command = sprintf(
+                self::COMMAND_FSWATCH,
+                escapeshellarg($path),
+                escapeshellarg($script),
+                $asBackground ? self::ARG_AS_BG : ''
+            );
+        }
         if ($returnCommand) {
             return $command;
         }
@@ -86,12 +110,8 @@ class CliCommands
         $taskName,
         $asBackground = false
     ) {
-        $bashScript = '#!/bin/bash'."\n".self::runTask($taskName, false, true);
-        $bashTmpFile = sys_get_temp_dir().'/limo_cli_task_'.md5(
-                $bashScript
-            ).'.sh';
-        file_put_contents($bashTmpFile, $bashScript);
-        self::run(sprintf('chmod +x %s', escapeshellarg($bashTmpFile)));
+        $command = self::runTask($taskName, false, true);
+        $bashTmpFile = self::createExecutableFileFromCommand($command);
 
         return self::runFsWatch($path, $bashTmpFile, $asBackground);
     }
@@ -113,6 +133,23 @@ class CliCommands
         );
 
         return self::run($command);
+    }
+
+    /**
+     * @param $command
+     *
+     * @return string
+     */
+    static private function createExecutableFileFromCommand($command)
+    {
+        $bashScript = '#!/bin/bash'."\n".$command;
+        $bashTmpFile = sys_get_temp_dir().'/limo_cli_task_'.md5(
+                $bashScript
+            ).'.sh';
+        file_put_contents($bashTmpFile, $bashScript);
+        self::run(sprintf('chmod +x %s', escapeshellarg($bashTmpFile)));
+
+        return $bashTmpFile;
     }
 
 }
